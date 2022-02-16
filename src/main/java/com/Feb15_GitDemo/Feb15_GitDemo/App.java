d98455b8d0433c0a84912b64d5fcdb178bccf113
AaishwaryaSuryawanshi <aishwarya.99.suryawanshi@gmail.com>
2022-02-16 20:37:14 +0530

    Second Commit

---
+++ src/main/java/com/Feb15_GitDemo/Feb15_GitDemo/App.java
@@ -0,0 +1,13 @@
+package com.Feb15_GitDemo.Feb15_GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/Feb15_GitDemo/Feb15_GitDemo/Test1.java
@@ -0,0 +1,11 @@
+package com.Feb15_GitDemo.Feb15_GitDemo;
+
+public class Test1 {
+		 
+		int i;
+		public void m1()
+		{
+			System.out.println("M1()");
+		}
+
+}
